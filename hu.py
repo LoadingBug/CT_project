7bac895f14839c624642706182a4b4ea5a38046e
loadingbug-t480 <loadingweb06@gmail.com>
2026-05-25 16:53:35 +0100

    working hu function using ct_detect to calculate mu_w

--- hu.py
+++ hu.py
@@ -1,6 +1,7 @@
 import numpy as np
 from attenuate import *
 from ct_calibrate import *
+from ct_detect import *
 
 def hu(p, material, reconstruction, scale):
 	""" convert CT reconstruction output to Hounsfield Units
@@ -8,10 +9,21 @@ def hu(p, material, reconstruction, scale):
 	Units, using the material coefficients, photon energy p and scale given."""
 
 	# use water to calibrate
+	n = reconstruction.shape[1]
+	thickness = n*scale
+	I_0 = ct_detect(p, material.coeff('Water'), 0)
+	I = ct_detect(p, material.coeff('Water'), thickness)
 
 	# put this through the same calibration process as the normal CT data
+	mu_w = -np.log(I / I_0) / thickness
 
 	# use result to convert to hounsfield units
-	# limit minimum to -1024, which is normal for CT data.
+	hu_reconstruction = 1000 * (reconstruction - mu_w) / mu_w
 
-	return reconstruction
+	# limit minimum to -1024, which is normal for CT data.
+	hu_reconstruction = np.maximum(hu_reconstruction, -1024)
+
+	# limit maximum to +3027
+	hu_reconstruction = np.minimum(hu_reconstruction, 3027)
+
+	return hu_reconstruction
